9e794146553002b78b9d568b2a2bf988735db6ee
Vera Prinsen <vera.prinsen@nedap.com>
2025-05-20 20:24:37 +0200

    Don't show column names when there is no data

--- src/App.jsx
+++ src/App.jsx
@@ -64,22 +64,24 @@ export default function App() {
         </div>
       </div>
       <h1>{userName}</h1>
-      <table className="games-table">
-        <tr>
-          <th>Name</th>
-          <th>Players</th>
-          <th>Duration (minutes)</th>
-        </tr>
-        {filteredData.map(game => {
-          return (
-            <tr key={game.id}>
-              <td className="game name">{game.name}</td>
-              <td className="game players">{game.minPlayers} - {game.maxPlayers}</td>
-              <td className="game time">{game.minPlayTime} - {game.maxPlayTime}</td>
-            </tr>
-          )
-        })}
-      </table>
+      {filteredData.length > 0 && (
+        <table className="games-table">
+          <tr>
+            <th>Name</th>
+            <th>Players</th>
+            <th>Duration (minutes)</th>
+          </tr>
+          {filteredData.map(game => {
+            return (
+              <tr key={game.id}>
+                <td className="game name">{game.name}</td>
+                <td className="game players">{game.minPlayers} - {game.maxPlayers}</td>
+                <td className="game time">{game.minPlayTime} - {game.maxPlayTime}</td>
+              </tr>
+            )
+          })}
+        </table>
+      )}
     </div>
   )
 }
